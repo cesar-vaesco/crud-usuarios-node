5f624e4d12cc199214d7aa3189bf03f9d089195c
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-06-28 16:52:09 -0500

    Testeando la ruta principal de la aplicación

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ const app = express();
 const PORT = 5000;
 app.use(express.json());
 
+app.get('/', (req, res) => res.send('Hola, esta es la página principal'))
+
 app.listen((PORT), () => {
 
-    console.log('\nServer started on port:'.cyan, ` http://localhost:${PORT}\n`.green );
+    console.log('\nServer started on port:'.cyan, ` http://localhost:${PORT}\n`.green);
 
 });
