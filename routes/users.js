27299f7992b10e6b4d77d640678051694f1bad5a
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-06-28 17:38:12 -0500

    Método post -> agregando usuario a db

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,35 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+    {
+        name: "John",
+        lastName: "Doe",
+        age: 25
+    },
+    {
+        name: "Jane",
+        lastName: "Doe",
+        age: 24
+    }
+
+]
+
 
 // http://localhost:5000/users/
 router.get('/', (req, res) => {
-    res.send('hello...')
+    res.send(users);
 });
+
+
+router.post('/', (req, res) => {
+
+    const user = req.body;
+
+    users.push(user);
+
+    res.send(`User with the name ${user.name} added to the databases!`);
+})
 
 
 export default router;
