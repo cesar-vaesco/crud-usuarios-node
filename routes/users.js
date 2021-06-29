072eeaac0e7bcb6b4759286f69eb4a6a7d8dd752
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-06-28 23:40:21 -0500

    Creando la funcionalidad del método patch - modificando parcialmente o totalmento el objeto

--- routes/users.js
+++ routes/users.js
@@ -43,7 +43,21 @@ router.delete('/:id', (req, res) => {
 
     users = users.filter((user) => user.id !== id)
 
-    res.send(`User with the ${ id } delete from the database`)
+    res.send(`User with the ${id} delete from the database`)
+});
+
+router.patch('/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, lastName, age } = req.body;
+    const user = users.find((user) => user.id === id);
+
+    if (name) user.name = name;
+    if (lastName) user.lastName = lastName;
+    if (age) user.age = age;
+
+    res.send(`User with the id ${id} is modificated`)
+
+
 });
 
 
